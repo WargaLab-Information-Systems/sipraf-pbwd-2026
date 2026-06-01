db0f4b307a6b083e7a9a68078eeb37c3ce992c74
HariSafiraSetyawati <harisafirasetyawati@gmail.com>
2026-06-01 20:36:41 +0700

    modified

--- pages/facilities/index.php
+++ pages/facilities/index.php
@@ -19,7 +19,7 @@ foreach ($facilities as $row) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIPRAF - FASILITAS</title>
+    <title>SIPRAF - Fasilitas</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
--- pages/users/index.php
+++ pages/users/index.php
@@ -10,7 +10,7 @@ $query = mysqli_query($conn, "SELECT * FROM users ORDER BY id ASC");
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Management</title>
+    <title>SIPRAF -User Management</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
