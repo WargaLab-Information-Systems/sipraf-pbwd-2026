15831dbac3671ebce8c48309bbc8247aa0244ce5
edwardnumberi56-cell <edwardnumberi56@gmail.com>
2026-06-02 15:44:54 +0700

    Feature/pages/auth/login/logout dll (#48)
    * pages/auth/Login dan Logout
    
    * fixx 3
    
    ---------
    
    Co-authored-by: Arif Rizqy <107555247+arifrizqy@users.noreply.github.com>

--- helper/db_conn.php
+++ helper/db_conn.php
@@ -1,18 +1,8 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pw   = "";
-$db   = "db_sipraf";
-
 $conn = new mysqli(
     'localhost',
     'root',
     '',
     'db_sipraf'
 );
-
-if ($conn->connect_error) {
-
-    die("Koneksi gagal: " . $conn->connect_error);
-}
